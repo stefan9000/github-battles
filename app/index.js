f2dc81d00754ee8c4916990f7ddb3ae78804ad1e
stefan9000 <stefanstanisavljevic@live.com>
2020-12-15 12:19:25 +0100

    Winner & Loser Defined

--- app/index.js
+++ app/index.js
@@ -1,13 +1,17 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import Popular from './components/Popular'
+import Battle from './components/Battle'
 import './index.css'
+
+
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        Hello World!
-      </div>
+        <div className='container'>
+          <Battle />
+        </div>
     )
   }
 }
